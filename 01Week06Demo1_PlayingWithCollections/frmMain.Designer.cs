8de16007f63f1ef8c11acb3271dc9d69ea137950
arseyrc <arseyrc@gmail.com>
2022-06-06 14:58:02 -0400

    Week 6 Demo 1

---
+++ 01Week06Demo1_PlayingWithCollections/frmMain.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace _01Week06Demo1_PlayingWithCollections
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Show Labels";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 265);
+            this.Controls.Add(this.button1);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Week 6 - Demo 1";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 01Week06Demo1_PlayingWithCollections/frmMain.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _01Week06Demo1_PlayingWithCollections
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        #region Global Variables
+        const int NumLabels = 10;
+        List<Label> TheLabels = new List<Label>();
+
+        // Lists, Queues, and Stacks are generic collections
+        // List is a object thrown in a box in random order
+        // Queue is FIFO lineup (like a line at a bar)
+        // Stack is LIFO pile (like a stack of books)
+        #endregion
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            Label myLabel = new Label();
+            myLabel.Name = "lblHeader";
+            myLabel.Text = "Week 6 Demo 1";
+            myLabel.AutoSize = false;
+            myLabel.Top = 0;
+            myLabel.Left = 0;
+            myLabel.Height = 40;
+            myLabel.Width = this.Width;
+
+            myLabel.Anchor = AnchorStyles.Top | AnchorStyles.Left |AnchorStyles.Right;
+            myLabel.BackColor = Color.Black;
+            myLabel.ForeColor = Color.White;
+
+            myLabel.TextAlign = ContentAlignment.MiddleCenter;
+            myLabel.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+
+            this.Controls.Add(myLabel);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Label lbl;
+
+            for (int i = 0; i < NumLabels; i++)
+            {
+                lbl = new Label();
+                lbl.Name = "lblMyLabel" + i.ToString();
+                lbl.Text = "Label " + i.ToString();
+
+                TheLabels.Add(lbl); // adding labels to the generic collection (List)
+            }
+
+            // do math stuff to organize it, but essentially then use a for each loop
+            int Left = 10;
+            int Top = button1.Top + button1.Height + 10;
+
+            foreach(Label lbl2 in TheLabels)
+            {
+                lbl2.Left = Left;
+                lbl2.Top = Top;
+                this.Controls.Add(lbl2);
+                
+                Left = 10; // would be calc if needed
+                Top += lbl2.Height + 5;
+            }
+
+        }
+    }
+}
