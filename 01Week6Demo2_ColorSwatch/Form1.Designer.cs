07c0106fba0f6edf41732c9fa320efc991a2dd9b
Clint <clint.macdonald@durhamcollege.ca>
2022-06-09 11:57:26 -0400

    week 6 - Demo 2 Color Swatch

---
+++ 01Week6Demo2_ColorSwatch/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace _01Week6Demo2_ColorSwatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nudNumberOfBoxes = new System.Windows.Forms.NumericUpDown();
+            this.btnRandomize = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumberOfBoxes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nudNumberOfBoxes
+            // 
+            this.nudNumberOfBoxes.Location = new System.Drawing.Point(13, 13);
+            this.nudNumberOfBoxes.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.nudNumberOfBoxes.Name = "nudNumberOfBoxes";
+            this.nudNumberOfBoxes.Size = new System.Drawing.Size(47, 27);
+            this.nudNumberOfBoxes.TabIndex = 0;
+            this.nudNumberOfBoxes.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // btnRandomize
+            // 
+            this.btnRandomize.Location = new System.Drawing.Point(67, 13);
+            this.btnRandomize.Name = "btnRandomize";
+            this.btnRandomize.Size = new System.Drawing.Size(94, 29);
+            this.btnRandomize.TabIndex = 1;
+            this.btnRandomize.Text = "Randomize";
+            this.btnRandomize.UseVisualStyleBackColor = true;
+            this.btnRandomize.Click += new System.EventHandler(this.btnRandomize_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExit.Location = new System.Drawing.Point(565, 516);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(94, 29);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClear.Location = new System.Drawing.Point(465, 516);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(94, 29);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 557);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnRandomize);
+            this.Controls.Add(this.nudNumberOfBoxes);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Color Swatch Picker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumberOfBoxes)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nudNumberOfBoxes;
+        private System.Windows.Forms.Button btnRandomize;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ 01Week6Demo2_ColorSwatch/Form1.cs
@@ -0,0 +1,150 @@
+﻿#region Header
+/*  Name:   Clint MacDonald
+ *  Date:   June 9, 2022
+ *  Title:  Week 6 - Demo 2 - Color Swatch Picker
+*/
+#endregion
+#region USING
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+#endregion
+
+namespace _01Week6Demo2_ColorSwatch
+{
+    public partial class Form1 : Form
+    {
+        #region Main Function
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Global Variables
+        const int MinDimSize = 3;
+        const int MaxDimSize = 8;
+        const int DefDimSize = 5;
+
+        const int BlockSize = 80;
+        const int GapSize = 5;
+        const int StartingXPos = 90;
+        const int StartingYPos = 50;
+        Label[,] colors;
+
+        #endregion
+
+        #region Events
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitialSetup();
+        }
+
+        private void btnRandomize_Click(object sender, EventArgs e)
+        {
+            ClearLabels();
+            CreateSwatches(Convert.ToInt32(nudNumberOfBoxes.Value));
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearLabels();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ColorLabel_Click(object sender, EventArgs e)
+        {
+            SetColor((Label)Convert.ChangeType(sender, typeof(Label)));
+        }
+        #endregion
+
+        #region Custom Methods
+        private void InitialSetup()
+        {
+            colors = new Label[MaxDimSize, MaxDimSize];
+            nudNumberOfBoxes.Minimum = MinDimSize;
+            nudNumberOfBoxes.Maximum = MaxDimSize;
+            nudNumberOfBoxes.Value = DefDimSize;
+        }
+
+        private void CreateSwatches(int dimSize)
+        {
+            for (int r = 0; r < dimSize; r++)
+            {
+                for (int c = 0; c < dimSize; c++)
+                {
+                    colors[r, c] = new Label();
+                    colors[r, c].Name = "label" + r.ToString() + c.ToString();
+                    colors[r, c].Top = StartingYPos + r * (BlockSize + GapSize);
+                    colors[r, c].Left = StartingXPos + c * (BlockSize + GapSize);
+                    colors[r, c].Height = BlockSize;
+                    colors[r, c].Width = BlockSize;
+
+                    SetColor(colors[r, c]);
+                    //colors[r, c].BackColor = Color.Yellow;
+                                        
+                    colors[r, c].Click += new System.EventHandler(this.ColorLabel_Click);
+
+                    this.Controls.Add(colors[r, c]);
+                }
+            }
+
+
+        }
+
+        private void ClearLabels()
+        {
+            for (int r = 0; r < MaxDimSize; r++)
+            {
+                for (int c = 0; c < MaxDimSize; c++)
+                {
+                    this.Controls.Remove(colors[r, c]);
+                }
+            }
+        }
+        
+        private void SetColor(Label l)
+        {
+            int red, green, blue, avg;
+            red = GetRandom(0, 255);
+            green = GetRandom(0, 255);
+            blue = GetRandom(0, 255);
+            avg = (red + green + blue) / 3;
+            if (avg < 127)
+                l.ForeColor = Color.White;
+            else
+                l.ForeColor = Color.Black;
+
+            l.BackColor = Color.FromArgb(red, green, blue);
+            l.Text = ConvertToHex(red, green, blue);
+        }
+
+
+
+        private int GetRandom(int minNum, int maxNum)
+        {
+            Random r = new Random();
+            return r.Next(minNum, maxNum);
+        }
+
+        private string ConvertToHex(int r, int g, int b)
+        {
+            string retVal = string.Empty;
+            retVal = "#" + r.ToString("X") + g.ToString("X") + b.ToString("X");
+            return retVal;
+        }
+        #endregion
+
+
+    }
+}
